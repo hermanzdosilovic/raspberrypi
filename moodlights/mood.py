2bbdf38f5ebfb0fdf72fc4805e1841e033fb2701
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-03-04 01:33:13 +0100

    New code for subset show

--- moodlights/mood.py
+++ moodlights/mood.py
@@ -5,77 +5,51 @@ from itertools import product
 
 GPIO.setmode(GPIO.BCM)
 
-gpios = [26, 19, 13, 6] # 5 (not active)
-leds = [] # blue, green, yellow, red, white (not active)
-intensity = [1, 1, 1, 1] # 0 <= x <= 1
-for gpio in gpios:
-  GPIO.setup(gpio, GPIO.OUT)
-  led = GPIO.PWM(gpio, 50)
-  led.start(0)
-  leds.append(led)
+LED_GPIO = [26, 19, 13, 6] # blue, green, yellow, red
+leds = []
 
-try:
-  while True:
-    # Sequence show
-    # If n is number of LEDs, numbered from 0 to n - 1, then this show will
-    # light all LEDs in interval [i, j] for every i, j: 0 <= i <= j < n
-    for intervalLength in range(1, len(leds) + 1):
-      for i in range(0, len(leds) + 1 - intervalLength):
-        for dutyCycle in range(0, 101):
-          for j in range(i, i + intervalLength):
-            leds[j].ChangeDutyCycle(dutyCycle * intensity[j])
-          sleep(0.07)
-        sleep(60)
-        for dutyCycle in range(100, -1, -1):
-          for j in range(i, i + intervalLength):
-            leds[j].ChangeDutyCycle(dutyCycle * intensity[j])
-          sleep(0.07)
+def fadein(led_indexes, intensity, sleep_time):
+  for dc in range(0, 101):
+    for i, val in enumerate(led_indexes):
+      leds[val].ChangeDutyCycle(dc * intensity[i])
+    sleep(sleep_time) 
 
-    # Subsets show
-    # If n in number of LEDs, then this show will light all 2^n subsets of LEDs
-    for n in range(1, 2**len(leds)):
-       i = 0
-       activate = []
-       while n != 0:
-         if n%2 == 1:
-           activate.append(i)
-         i += 1
-         n /= 2
-       for dutyCycle in range(0, 101):
-         for index in activate:
-           leds[index].ChangeDutyCycle(dutyCycle * intensity[index])
-         sleep(0.07)
-       sleep(60)
-       for dutyCycle in range(100, -1, -1):
-         for index in activate:
-           leds[index].ChangeDutyCycle(dutyCycle * intensity[index])
-         sleep(0.07)
-   
-    # If n in number of LEDs, then this show will light all 2^n subsets of LEDs
-    # with 10 possibilities of light intensity
-    intensity_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-    for intensity in product(intensity_list, repeat=len(leds)):
-      for n in range(1, 2**len(leds)):
-         i = 0
-         activate = []
-         while n != 0:
-           if n%2 == 1:
-             activate.append(i)
-           i += 1
-           n /= 2
-         for dutyCycle in range(0, 101):
-           for index in activate:
-             leds[index].ChangeDutyCycle(dutyCycle * intensity[index])
-           sleep(0.07)
-         sleep(60)
-         for dutyCycle in range(100, -1, -1):
-           for index in activate:
-             leds[index].ChangeDutyCycle(dutyCycle * intensity[index])
-           sleep(0.07)
+def fadeout(led_indexes, intensity, sleep_time):
+  for dc in range(100, -1 , -1):
+    for i, val in enumerate(led_indexes):
+      leds[val].ChangeDutyCycle(dc * intensity[i])
+    sleep(sleep_time) 
 
-except KeyboardInterrupt:
-  pass
+def subset_show():
+  intensity_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+  for subset in range(1, 2**len(leds)):
+    index = 0
+    led_indexes = []
+    while subset != 0:
+      if subset%2 == 1:
+        led_indexes.append(index)
+      index += 1
+      subset /= 2 
+    
+    fadein(led_indexes, [1 for x in range(0, len(led_indexes))], 0.07) 
+    fadeout(led_indexes, [1 for x in range(0, len(led_indexes))], 0.07) 
 
-for led in leds:
-  led.stop()
-GPIO.cleanup()
+def main():
+  for gpio in LED_GPIO:
+    GPIO.setup(gpio, GPIO.OUT)
+    led = GPIO.PWM(gpio, 50)
+    led.start(0) # start with 0 duty cycle
+    leds.append(led)
+  
+  try:
+    while True:
+      subset_show()
+  except:
+    pass
+
+  for led in leds:
+    led.stop() 
+  GPIO.cleanup()
+
+if __name__ == "__main__":
+  main()
